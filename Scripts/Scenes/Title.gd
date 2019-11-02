7fc15b9bbba6d66ebd26b7da0e8092cd0b64518c
Duncan Sparks <duncan.sprks@gmail.com>
2019-11-01 23:06:36 -0700

    Menu stuff

--- Scripts/Scenes/Title.gd
+++ Scripts/Scenes/Title.gd
@@ -1,12 +1,45 @@
 extends Node2D
 
+var nav_pos: int = -1
+
 onready var but1 := get_node("Button1")
 onready var but2 := get_node("Button2")
 onready var but3 := get_node("Button3")
 
+onready var buttons = [but1, but2, but3]
+
+onready var anim_player_blink := $Cosmo/AnimationPlayer
+
 func _ready():
 	Controller.draw_overlay(false)
 	Player.hide()
+	
+	
+func _process(delta):
+	if Input.is_action_just_pressed("sys_up"):
+		if nav_pos != -1:
+			buttons[nav_pos]._on_areaHover_mouse_exited()
+			nav_pos = wrapi(nav_pos - 1, 0, 3)
+		else:
+			nav_pos = 0
+		buttons[nav_pos]._on_AreaHover_mouse_entered()
+	
+	if Input.is_action_just_pressed("sys_down"):
+		if nav_pos != -1:
+			buttons[nav_pos]._on_areaHover_mouse_exited()
+			nav_pos = wrapi(nav_pos + 1, 0, 3)
+		else:
+			nav_pos = 0
+		buttons[nav_pos]._on_AreaHover_mouse_entered()
+
+# =====================================================================
+
+func randomize_anim_player_speed():
+	anim_player_blink.set_speed_scale(rand_range(0.25, 0.5))
+	
+	
+func reset_anim_player_speed():
+	anim_player_blink.set_speed_scale(1.2)
 
 # =====================================================================
 
