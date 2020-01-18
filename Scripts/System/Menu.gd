fc143ff45399400ea0e5ff6ee72cc0f46e0b283d
Duncan Sparks <duncan.sprks@gmail.com>
2020-01-18 12:07:56 -0800

    Fix the inn

--- Scripts/System/Menu.gd
+++ Scripts/System/Menu.gd
@@ -7,8 +7,9 @@ var active := false
 onready var but1 := $CanvasLayer/Button1 as ButtonUF
 onready var but2 := $CanvasLayer/Button2 as ButtonUF
 onready var but3 := $CanvasLayer/Button3 as ButtonUF
+onready var but4 := $CanvasLayer/Button4 as ButtonUF
 
-onready var buttons: Array = [but1, but2, but3]
+onready var buttons: Array = [but1, but2, but3, but4]
 
 onready var anim_player := $AnimationPlayer as AnimationPlayer
 
@@ -48,12 +49,17 @@ func _on_Button3_clicked():
 	but3.set_hover(false)
 	for but in buttons:
 		but.appear()
+		
+		
+func _on_Button4_clicked():
+	Controller.select_menu_button(buttons, but4.get_name(), true)
+	anim_player.play("Disappear 2")
 	
 
 func _on_AnimationPlayer_animation_finished(anim_name: String):
 	if anim_name == "Appear":
 		active = true
-	elif anim_name == "Disappear":
+	elif anim_name == "Disappear" or anim_name == "Disappear 2":
 		Controller.set_menu_open(false)
 		Player.set_state(Player.PlayerState.Move)
 		queue_free()
