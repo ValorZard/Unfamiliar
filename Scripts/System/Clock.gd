adf108e78f65689e5bd76e1f5f185eb56bdec2f4
Duncan Sparks <duncan.sprks@gmail.com>
2020-01-29 07:35:52 -0800

    Time to switch to 3.2

--- Scripts/System/Clock.gd
+++ Scripts/System/Clock.gd
@@ -1,13 +1,13 @@
 extends Sprite
 
-const ClockSprites: Dictionary = {
+var ClockSprites: Dictionary = {
 	Controller.GameTime.SixThirty: "res://Sprites/Props/Interiors/IN_Clock_630.png",
 	Controller.GameTime.SevenThirty: "res://Sprites/Props/Interiors/IN_Clock_7.png",
 	Controller.GameTime.Eight: "res://Sprites/Props/Interiors/IN_Clock_8.png",
 	Controller.GameTime.Nine: "res://Sprites/Props/Interiors/IN_Clock_9.png",
 	Controller.GameTime.Twelve: "res://Sprites/Props/Interiors/IN_Clock_12.png",
 	Controller.GameTime.Three: "res://Sprites/Props/Interiors/IN_Clock_3.png",
-	Controller.GameTime.Five: "res://Sprites/Props/Interiors/IN_Clock_5.png",
+	Controller.GameTime.Five: "res://Sprites/Props/Interiors/IN_Clock_5.png"
 }
 
 func _ready():
