b95255257d4eb077f75cc92877ea746b0ee010b9
Duncan Sparks <duncan.sprks@gmail.com>
2020-04-17 17:59:14 -0700

    Finish Yaga cutscene (Thanks VGDC viewers)

--- Scripts/Scenes/Los Muertos/Scene_Deli_TL.gd
+++ Scripts/Scenes/Los Muertos/Scene_Deli_TL.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export(NodePath) var npc_yaga1: NodePath
 export(NodePath) var npc_yaga2: NodePath
+export(NodePath) var plate: NodePath
 
 func _ready():
 	if Controller.flag("scn_deli_yaga") == 1:
@@ -10,4 +11,5 @@ func _ready():
 		yaga.set_position(Vector2(134, 66))
 		yaga.set_direction(2)
 		yaga.show()
+		(get_node(plate) as Sprite).show()
 		Player.set_sprite_override(false)
